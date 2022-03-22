2a5d16a99111afd914000e8342def7747686171b
karktya <didkovskayacatherine@yandex.ru>
2022-03-22 14:37:17 +0200

    add template

--- app/routes/main_route.py
+++ app/routes/main_route.py
@@ -1,7 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_route = Blueprint("main_route", __name__)
 
 @main_route.route("/")
-def hello():
-    return 'HELLO'
+@main_route.route('/index')
+def index():
+    puctures = {
+                "contents": [
+                    {
+                        "title": "https://proprikol.ru/wp-content/uploads/2021/01/sobachki-krasivye-kartinki-13.jpg",
+                        "content": "$5"
+                    },
+                    {
+                        "title": "https://proprikol.ru/wp-content/uploads/2021/01/sobachki-krasivye-kartinki-13.jpg",
+                        "content": "$6"
+                    },
+                    {
+                        "title": "https://proprikol.ru/wp-content/uploads/2021/01/sobachki-krasivye-kartinki-13.jpg",
+                        "content": "$4"
+                    },
+                    {
+                        "title": "https://kartinkin.net/uploads/posts/2021-07/thumbs/1625296547_11-kartinkin-com-p-french-dog-yeda-krasivo-foto-57.jpg",
+                        "content": "$5"
+                    },
+                    {
+                        "title": "https://kartinkin.net/uploads/posts/2021-07/thumbs/1625296547_11-kartinkin-com-p-french-dog-yeda-krasivo-foto-57.jpg",
+                        "content": "$6"
+                    },
+                    {
+                        "title": "https://kartinkin.net/uploads/posts/2021-07/thumbs/1625296547_11-kartinkin-com-p-french-dog-yeda-krasivo-foto-57.jpg",
+                        "content": "$4"
+                    }
+
+                        ]
+                }
+    return render_template('index.html', pics=puctures)
