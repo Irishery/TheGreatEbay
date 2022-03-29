10ab5a992bdd3823992ec6c80a481cfb68bb5c93
Irishery <markirill21@gmail.com>
2022-03-29 17:49:40 +0300

    added db models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app)
 
     from .routes.main_route import main_route
     app.register_blueprint(main_route)
---
+++ app/models.py
@@ -0,0 +1,77 @@
+from app import db
+from dataclasses import dataclass
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@dataclass
+class User(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    first_name: str = db.Column(db.String(120), nullable=False)
+    last_name: str = db.Column(db.String(120), nullable=False)
+    login: str = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash: str = db.Column(db.String(255), nullable=False)
+    is_seller: bool = db.Column(db.Boolean, default=False)
+    desription: str = db.Column(db.String(255), default=None)
+    gender: str = db.Column(db.String(80))
+    payment_type: str = db.Column(db.String(120), default=None)
+    total_spending: int = db.Column(db.Integer, default=0)
+    registered_datetime: datetime = db.Column(db.DateTime(timezone=True),
+                                              nullable=False)
+    last_enter: datetime = db.Column(db.DateTime(timezone=True), nullable=False)
+
+    def __init__(self, first_name, last_name, login, password, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login = login
+        self.password = self.set_password(password)
+        self.gender = gender
+        self.registered_datetime = datetime.now()
+
+    def get_id(self):
+        return str(self.id)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+@dataclass
+class Seller(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    user_id: int = db.Column(db.Integer, db.ForeignKey('user.id'),
+                                         nullable=False)
+    selled_count: int = db.Column(db.Integer, default=0)
+    active_products: int = db.Column(db.Integer, default=0)
+    total_income: int = db.Column(db.Integer, default=0)
+    open_datetime: datetime = db.Column(db.DateTime(timezone=True))
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.open_datetime = datetime.now()
+
+
+@dataclass
+class Product(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    seller_id: int = db.Column(db.Integer, db.ForeignKey('seller.id'),
+                                         nullable=False)
+    name: str = db.Column(db.String(180), nullable=False)
+    description: str = db.Column(db.String(255), nullable=False)
+    type: str = db.Column(db.String(180), nullable=False)
+    cost: int = db.Column(db.Integer, nullable=False)
+    picture: str = db.Column(db.String(255), default=None)
+    open_datetime: datetime = db.Column(db.DateTime, nullable=False)
+    selled_datetime: datetime = db.Column(db.DateTime, default=None)
+    is_selled: bool = db.Column(db.Boolean, default=False)
+
+    def __init__(self, seller_id, name, description, type, cost, picture=None):
+        self.seller_id = seller_id
+        self.name = name
+        self.description = description
+        self.type = type
+        self.cost = cost
+        if picture:
+            self.picture = picture
